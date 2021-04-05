5f52063cec7b1c38596185dab3a9295861ab2444
Ilona <ilonarus@hotmail.com>
2021-04-05 11:20:38 +0200

    Created submit post

--- src/app.js
+++ src/app.js
@@ -4,8 +4,31 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
+    .catch(error => console.log(error));
+}
+
+
+// Submit post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  // Create post
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      getPosts();
+    })
     .catch(error => console.log(error));
 }
